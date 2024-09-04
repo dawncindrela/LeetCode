91612789c4998f309dc79dfcc777dc1f823faccc
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-09-05 01:20:14 +0530

    Time: 1 ms (46.89%), Space: 41 MB (94.16%) - LeetHub

---
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public double average(int[] salary) 
+    {
+        Arrays.sort(salary);
+        double sum=0;
+        int n=salary.length;
+        for(int i=1;i<n-1;i++)
+        {
+            sum+=salary[i];
+        }
+        double avg=sum/(n-2);
+        return avg;
+    }
+}
