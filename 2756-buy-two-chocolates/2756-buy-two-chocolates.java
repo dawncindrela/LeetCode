9496be1d0d388a24f194325db578711c93ff6ec0
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-09-27 19:00:23 +0530

    Time: 2 ms (75.64%), Space: 44.6 MB (20.54%) - LeetHub

---
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int buyChoco(int[] prices, int money) 
+    {
+        Arrays.sort(prices);
+        int p=prices[0]+prices[1];
+        if(p<=money)
+        {
+            return (money - p);
+        }
+        else if(p>money)
+        {
+            return money;
+        }
+        return 0;
+    }
+}                                                                                                                                           
