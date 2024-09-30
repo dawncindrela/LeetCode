35e3bcd90bdbfaba2e6165dd2f4bc61447cbb01f
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-09-30 13:36:11 +0530

    Time: 3 ms (89.76%), Space: 44.5 MB (47.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,6 +2,12 @@ class Solution
 {
     public boolean isAnagram(String s, String t) 
     {
+        int ls=s.length();
+        int lt=t.length();
+        if(ls!=lt)
+        {
+            return false;
+        }
         char[] sa=s.toCharArray();
         char[] ta=t.toCharArray();
         Arrays.sort(sa);
