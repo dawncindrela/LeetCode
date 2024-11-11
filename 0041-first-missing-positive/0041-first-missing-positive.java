3ef07495c6248e8d328fac29223db99f07b9c15e
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-11-11 21:09:11 +0530

    Time: 14 ms (26.86%), Space: 56.6 MB (81.86%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int firstMissingPositive(int[] nums) 
+    {
+        Arrays.sort(nums);
+        int target = 1;
+        for (int n : nums) 
+        {
+            if (n == target) 
+            {
+                target++;
+            } 
+            else if (n > target) 
+            {
+                return target;
+            }
+        }
+        return target;        
+    }
+}
