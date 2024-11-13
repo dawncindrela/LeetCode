aba01659fd087db1df1151fadbd90127951fe20b
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-11-13 20:32:26 +0530

    Time: 1 ms (99.38%), Space: 41.7 MB (98.54%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.java
+++ 3636-check-balanced-string/3636-check-balanced-string.java
@@ -7,7 +7,7 @@ class Solution
         int odd = 0;
         for(int i=0;i<n;i++)
         {
-            if(i % 2 == 1)
+            if(i % 2 == 0)
             {
                 even += (num.charAt(i)-'0');
             }
