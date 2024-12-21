590aa79e7c5fe3b290fd103c212673d706012525
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2024-12-21 17:54:57 +0530

    Time: 0 ms (100%), Space: 41.1 MB (26.38%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int smallestNumber(int n) 
+    {
+        int x = n;
+        while (true) 
+        {
+            if ((x & (x + 1)) == 0) 
+            {
+                return x;
+            }
+            x++;
+        }
+    }
+}
