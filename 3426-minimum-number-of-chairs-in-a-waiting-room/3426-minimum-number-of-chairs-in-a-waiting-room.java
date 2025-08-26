ffed1e81a96f4129b5ec6f59b6caa08b2b01700a
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2025-08-26 18:06:24 +0530

    Time: 2 ms (5.27%), Space: 42.6 MB (13.71%) - LeetHub

---
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public int minimumChairs(String s) 
+    {
+        while(s.contains("LE"))
+        {
+            s=s.replace("LE","");
+        }
+        s=s.replace("L","");
+        
+        return s.length();
+    }
+}
