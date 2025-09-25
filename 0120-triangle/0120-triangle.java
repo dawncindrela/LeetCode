2da47c81e1dea136c8b54ecf7d6f55e854326300
dawncindrela <144470354+dawncindrela@users.noreply.github.com>
2025-09-25 23:25:39 +0530

    Time: 1 ms (99.85%), Space: 44.6 MB (99.92%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -10,8 +10,10 @@ class Solution {
     {
         if(i==triangle.size())
          return 0;
+
         if(val[i][j]!=-10001) 
          return val[i][j];
+
        int a= triangle.get(i).get(j)+findFn(triangle,i+1,j,val);
        int b= triangle.get(i).get(j)+findFn(triangle,i+1,j+1,val);
        return val[i][j]=Math.min(a,b);
